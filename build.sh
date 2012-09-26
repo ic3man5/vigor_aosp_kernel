08477a4afaa8a49ea7d3eaa9e3e46c01ad9b25a2
ic3man5 <ic3man5@gmail.com>
2012-09-25 20:30:29 -0400

    Can skip building by using --skip-build parameter now.
    Signed-off-by: ic3man5 <ic3man5@gmail.com>

--- build.sh
+++ build.sh
@@ -7,19 +7,21 @@ TOOLCHAIN_PREFIX=arm-none-eabi-
 # setup config file version
 sed -i s/CONFIG_LOCALVERSION=\"-ic3man5-.*\"/CONFIG_LOCALVERSION=\"-ic3man5-${2}AOSP\"/ .config
 
-#compile the kernel
-make -j$CPU_COUNT ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
-if [ $? -ne 0 ]; then
-	echo -ne "\n\nWARNING: Build failed, stopping...\n\n"
-	exit;
+#compile the kernel unless --skip-build is specified.
+if [ "$1" != "--skip-build" ]; then
+	make -j$CPU_COUNT ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
+	if [ $? -ne 0 ]; then
+		echo -ne "\n\nWARNING: Build failed, stopping...\n\n"
+		exit;
+	fi
+
+	# clean up modules a bit
+	find . -name "*.ko" | xargs $TOOLCHAIN/${TOOLCHAIN_PREFIX}strip --strip-unneeded
+
+	# Move modules.
+	find . -name "*.ko" -exec cp "{}" ./modules/ \;
+	echo "Modules have been moved to ./modules/"
 fi
-
-# clean up modules a bit
-find . -name "*.ko" | xargs $TOOLCHAIN/${TOOLCHAIN_PREFIX}strip --strip-unneeded
-
-find . -name "*.ko" -exec cp "{}" ./modules/ \;
-echo "Modules have been moved to ./modules/"
-
 
 read -p "Would you like to create a boot.img file (requires phone/adb) (y/n)? " answer
 if [ "$answer" = "y" ]; then
